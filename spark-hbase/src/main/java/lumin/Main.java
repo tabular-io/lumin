51775f924b3eafa3ce9acf86329e163c3bff6b0d
Bryan Keller <bryanck@gmail.com>
2022-10-01 15:23:04 -0700

    added comment

--- spark-hbase/src/main/java/lumin/Main.java
+++ spark-hbase/src/main/java/lumin/Main.java
@@ -8,8 +8,8 @@ public class Main {
   public static void main(String[] args) {
     SparkConf conf =
         new SparkConf()
-            .setMaster("local")
-            .setAppName("tabular-analyzer")
+            .setMaster("local") // TODO: remove
+            .setAppName("tsdb-import")
             .set("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
             .set(
                 "spark.hadoop.fs.s3a.aws.credentials.provider",
@@ -20,7 +20,7 @@ public class Main {
             .set("spark.sql.catalog.tabular.credential", System.getenv("TABULAR_CREDS"))
             .set("spark.sql.defaultCatalog", "tabular");
 
-    SparkSession spark = SparkSession.builder().config(conf).appName("lumin-import").getOrCreate();
+    SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
     String dataDir = "s3://tabular-lumin/data/tsdb/";
     String dataTable = "temp.lumin_data";
     String uidDir = "s3://tabular-lumin/data/tsdb-uid/";
